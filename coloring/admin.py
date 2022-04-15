32eeb7e50d2ece42c5bb4eb92a0b00c8f3314d3b
ashriftman <44977362+ashriftman@users.noreply.github.com>
2022-04-15 00:09:02 +0000

    initial commit

--- coloring/admin.py
+++ coloring/admin.py
@@ -1,9 +1,14 @@
+
 from django.contrib import admin
+# TODO: delete author later
 from coloring.models import Author
+from coloring.models import User
+from coloring.models import Posting
 
 # Register your models here.
 # https://docs.djangoproject.com/en/4.0/ref/contrib/admin/#modeladmin-objects
 
-admin.site.register(Author)
+admin.site.register(User)
+admin.site.register(Posting)
 
 
--- coloring/models.py
+++ coloring/models.py
@@ -1,8 +1,34 @@
 from django.db import models
 
-# The Author model has been created for you
+# todo: delete Author later
 class Author(models.Model):
   name = models.CharField(max_length=50)
+  
+class User(models.Model):
+  username = models.CharField(max_length=50)
 
-# Create the Drawing model
-
+class Posting(models.Model):
+  QTY_CHOICES = [
+    ('UN', 'Units'),
+    ('LB', 'Pounds'),
+    ('OZ', 'Ounces'),
+    ('PT', 'Pints'),
+    ('GL', 'Gallons'),
+]
+  item_name = models.CharField(max_length=70)
+  listing_user = models.ForeignKey(User, on_delete=models.CASCADE, null = True)
+  qty = models.IntegerField()
+  qty_units = models.CharField(
+        max_length=2,
+        choices=QTY_CHOICES,
+        default='UN'
+    )
+  best_by = models.DateField()
+  private = models.BooleanField(default = False)
+  description = models.CharField(max_length=300)
+  active = models.BooleanField(default = True)
+  # tags
+  unopened = models.BooleanField(default = False)
+  og_packaging = models.BooleanField(default = False)
+  store_bought = models.BooleanField(default = False)
+  homemade = models.BooleanField(default = False)
