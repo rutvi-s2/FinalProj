4fd2dee892476bd2ad5b2ff045616dee1e5e14cd
olaylay18 <66924752+olaylay18@users.noreply.github.com>
2022-04-18 17:13:56 +0000

    made database accessible again -- added wsateless to url

--- coloring/urls.py
+++ coloring/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('<slug:username>/newlisting/', views.newlisting),
-  path('<slug:username>/friends/', views.friends),
-  path('<slug:username>/', views.index),
+  path('wasteless/<slug:username>/newlisting/', views.newlisting),
+  path('wasteless/<slug:username>/friends/', views.friends),
+  path('wasteless/<slug:username>/', views.index),
   # path('wasteless/', views.index),
   path('', views.index)
 ]
