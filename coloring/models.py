817cec658f9685ee1731a7cf60b3a21a1c790a6d
olaylay18 <66924752+olaylay18@users.noreply.github.com>
2022-04-18 17:10:59 +0000

    ran models lines

--- coloring/models.py
+++ coloring/models.py
@@ -6,6 +6,12 @@ class Author(models.Model):
   
 class User(models.Model):
   username = models.CharField(max_length=50)
+  friends = models.JSONField(null=True)
+  saved = models.JSONField(null=True)
+  claimed = models.JSONField(null=True)
+  rating = models.IntegerField(null=True)
+  total = models.IntegerField(null=True)
+  verified = models.BooleanField(default = False)
 
 class Posting(models.Model):
   QTY_CHOICES = [
@@ -17,13 +23,14 @@ class Posting(models.Model):
 ]
   item_name = models.CharField(max_length=70)
   listing_user = models.ForeignKey(User, on_delete=models.CASCADE, null = True)
-  qty = models.IntegerField()
+  # claimed_by = models.ForeignKey(User, on_delete=models.CASCADE, null = True)
+  qty = models.IntegerField(null=True)
   qty_units = models.CharField(
         max_length=2,
         choices=QTY_CHOICES,
         default='UN'
     )
-  best_by = models.DateField()
+  best_by = models.DateField(null=True)
   private = models.BooleanField(default = False)
   description = models.CharField(max_length=300)
   active = models.BooleanField(default = True)
@@ -32,3 +39,4 @@ class Posting(models.Model):
   og_packaging = models.BooleanField(default = False)
   store_bought = models.BooleanField(default = False)
   homemade = models.BooleanField(default = False)
+  
